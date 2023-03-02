43175dfad030e65c5ffb6a071deb9f5a40b2df29
pooyasadeghi <pouyasadeghi2012@gmail.com>
2023-03-02 14:40:08 +0330

    Add user/user_data parser

--- codes/infra/user.h
+++ codes/infra/user.h
@@ -1,7 +1,3 @@
-//
-// Created by Pooya Sadeghi on 2023-02-28.
-//
-
 #ifndef SRC_USER_H
 #define SRC_USER_H
 #include "filehandler.h"
@@ -11,7 +7,15 @@
 #define DEFAULT_USERS_PATH "./config/UsersInfo.json"
 
 typedef struct UserData{
-
+    int id;
+    std::string username;
+    std::string password;
+    bool privilege; // user is admin
+    int account_balance; // stores as string in json fila, why??
+    std::string phone_number;
+    std::string address;
 } UserData;
+
+std::vector<UserData> get_users_data(std::string path);
 
 #endif //SRC_USER_H
--- codes/main.cpp
+++ codes/main.cpp
@@ -1,18 +1,18 @@
 #include "infra/server.h"
 #include "infra/hotel.h"
+#include "infra//user.h"
 #include <iostream>
 #include <glog/logging.h>
 
 void signalHandler(int signum) {
-    LOG(INFO) << "Received signal " << signum << ", terminating program.";
+    LOG(INFO) << "Received signal " << signum << ", terminating the program...";
     google::ShutdownGoogleLogging();
     exit(signum);
 }
 
-int main() {
-    google::InitGoogleLogging("Server");
+int main(int argc, char **argv) { // using just for test functionalities
+    google::InitGoogleLogging(argv[0]);
     signal(SIGINT, signalHandler);
-    signal(SIGTERM, signalHandler);
     FLAGS_colorlogtostderr = true;
     FLAGS_log_prefix = true;
     FLAGS_logtostderr = true;
@@ -25,6 +25,14 @@ int main() {
 
 
     auto h = get_hotel_information(DEFAULT_HOTEL_PATH);
+    for (auto _h : h){
+        cout << _h.room_number << ' ' << _h.max_capacity - _h.current_capacity << endl;
+    }
+
+    auto u = get_users_data(DEFAULT_USERS_PATH);
+    for (auto _u:u){
+        cout << _u.id << ' ' << (_u.privilege ? _u.username : _u.phone_number) << endl;
+    }
 
     google::ShutdownGoogleLogging();
     return 0;
