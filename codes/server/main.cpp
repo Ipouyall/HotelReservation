8a30116e0401bd197e5e72bd56d13569afde2b4e
pooyasadeghi <pouyasadeghi2012@gmail.com>
2023-03-07 08:37:57 +0330

    Refactore server

--- codes/server/main.cpp
+++ codes/server/main.cpp
@@ -21,33 +21,12 @@
 
 struct Client{
     int client_fd;
-    bool active;
-};
-Client* newClient() {
-    Client* new_client = new Client();
-    if (new_client == nullptr)
-        exit(EXIT_FAILURE);
-    new_client->active = false;
-    new_client->client_fd = -1;
-    return new_client;
-}
 
-Client* findClientByFd(const std::vector<Client*>& clients, int fd) {
-    for (const auto& client : clients) {
-        if (client->client_fd == fd) {
-            return client;
-        }
-    }
-    return nullptr;
-}
-void addNewClient(int client_fd, std::vector<Client*>& clients, int sock_fd, bool active) {
-    auto client = findClientByFd(clients, client_fd);
-    if (client == nullptr) {
-        clients.push_back(newClient());
-        client = clients.back();
-    }
-    client->client_fd = sock_fd;
-    client->active = active;
+    Client(int fd){ client_fd = fd;}
+};
+
+void add_new_client(std::vector<Client>& clients, int sock_fd) {
+    clients.push_back(Client(sock_fd));
 }
 
 void signalHandler(int signum) {
@@ -67,10 +46,9 @@ int main(int argc, char *argv[]){
 
     signal(SIGINT, signalHandler);
 
-    std::vector<Client*> clients;
-
+    std::vector<Client> clients;
     int server_fd, new_socket, max_sd;
-    char buffer[1024] = {0};
+    char buffer[1024];
     std::string bufferString;
     fd_set master_set, working_set;
     auto server_info = get_server_config(DEFAULT_SERVER_PATH);
@@ -83,7 +61,6 @@ int main(int argc, char *argv[]){
     LOG(INFO) << "Server is running...";
     while (true) {
         working_set = master_set;
-        memset(buffer, 0, 1024);
         select(FD_SETSIZE, &working_set, NULL, NULL, NULL);
         for(int i = 0;i < FD_SETSIZE;i++){
             if (FD_ISSET(i, &working_set)) {
@@ -91,35 +68,25 @@ int main(int argc, char *argv[]){
                     new_socket = acceptClient(i);
                     if(new_socket!=-1)
                         FD_SET(new_socket, &master_set);
-                    std::string empty = "";
-                    addNewClient(new_socket, clients, new_socket, true);
+                    add_new_client(clients, new_socket);
                 }
-                else{
+                else {
+                    memset(buffer, 0, 1024);
                     int bytes_received = recv(i , buffer, 1024, 0);
-                    if (bytes_received == 0) {
-                        Client* client = findClientByFd(clients, i);
-                        client->active = false;
+                    if (bytes_received == 0) { // Client has left
                         close(i);
                         FD_CLR(i, &master_set);
                         LOG(INFO) << "Client gone";
                         continue;
                     }
 
-                    bufferString = buffer;
+                    std::cout << "client fd = " << i << " said = " << buffer;
 
                     std::string response = "ok";
-
                     if(send(i, response.c_str(), strlen(response.c_str()), 0) != -1)
                         LOG(INFO) << "Your response sent";
                     else
                         LOG(WARNING) << "Error on sending your Response!";
-
-
-                    std::istringstream iss(bufferString);
-                    std::string nameString;
-                    std::getline(iss, nameString, ' ');
-
-                    LOG(INFO) << "client fd = " << i << " said = " << buffer;
                 }
             }
         }
